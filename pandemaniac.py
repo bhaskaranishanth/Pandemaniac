2926f9afa6455f0fb176b5040d416d732ff7416f
Nishanth Bhaskara <Nishanth@Nishanths-MacBook-Pro.local>
2017-02-14 00:23:33 -0800

    Added graphs in graphs folder

--- pandemaniac.py
+++ pandemaniac.py
@@ -3,6 +3,7 @@ from pprint import pprint
 import networkx as nx
 import matplotlib.pyplot as plt
 import sim
+import numpy as np
 
 def run(file_name):
     f = open(file_name, 'r')
@@ -25,7 +26,8 @@ def run(file_name):
     # nx.draw(G)
     # plt.show()
 
-    triangle_info = nx.traingles(G)
+
+    triangle_info = nx.triangles(G)
     
 
 
@@ -33,7 +35,23 @@ def run(file_name):
     strategies = {'s1' : [0, 1], 's2' : [5, 6]}
     output = sim.run(nx.to_dict_of_dicts(G), strategies)
     print output
+    return G
 
+G = run('testgraph1.json')
+dict_c_coeff = nx.clustering(G)
+coeff_seq = sorted(dict_c_coeff.values(),reverse = True)
+hist, bins = np.histogram(coeff_seq, bins = 50)
+width = 0.7 * (bins[1] - bins[0])
+center = (bins[:-1] + bins[1:]) / 2
+plt.bar(center, hist, align='center', width=width)
+plt.title('Histogram Of Node Clustering Coefficients')
+plt.ylabel("Count")
+plt.xlabel("Clustering Coefficients")
+plt.show()
+cdf = np.cumsum(hist)
+yvals = np.arange(len(coeff_seq))/float(len(coeff_seq))
+yvals = yvals
+plt.plot(coeff_seq, yvals)
+plt.title('CCDF Of Node Clustering Coefficients')
+plt.show()
 
-
-run('testgraph1.json')
