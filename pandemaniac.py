0be854f014f55992f47528ef2a86249ae85083c9
Ramruthwick Pathireddy <rpathire@caltech.edu>
2017-02-13 23:57:21 -0800

    Parsed the data structure into a better format

--- pandemaniac.py
+++ pandemaniac.py
@@ -1,12 +1,24 @@
 import json
 from pprint import pprint
+import networkx as nx
 
 def run(file_name):
     f = open(file_name, 'r')
-    graph_data = json.loads(f.read())
+    data = json.loads(f.read())
 
+    graph_data = {}
+    for k, v in data.iteritems():
+        graph_data[int(k)] = [int(x) for x in v]
 
     pprint(graph_data)
+
+    # G = nx.Graph()
+    # for k, v in graph_data.iteritems():
+    #     for n in v:
+    #         G.add_edge(k, v)
+
+
+    # pprint(graph_data)
 
 
 
