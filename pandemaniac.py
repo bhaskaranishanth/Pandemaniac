bd3ed2e7bed557f82c054078a890843ca5db275c
Ramruthwick Pathireddy <rpathire@caltech.edu>
2017-02-14 00:17:16 -0800

    Added simulation strategy to the main file

--- pandemaniac.py
+++ pandemaniac.py
@@ -2,6 +2,7 @@ import json
 from pprint import pprint
 import networkx as nx
 import matplotlib.pyplot as plt
+import sim
 
 def run(file_name):
     f = open(file_name, 'r')
@@ -21,8 +22,18 @@ def run(file_name):
 
     # pprint(graph_data)
     # print G.nodes()
-    nx.draw(G)
-    plt.show()
+    # nx.draw(G)
+    # plt.show()
+
+    triangle_info = nx.traingles(G)
+    
+
+
+    # Strategy
+    strategies = {'s1' : [0, 1], 's2' : [5, 6]}
+    output = sim.run(nx.to_dict_of_dicts(G), strategies)
+    print output
+
 
 
 run('testgraph1.json')
