cefea7eeab6d8b4bfa62267e425e74e3bc885051
Ramruthwick Pathireddy <rpathire@caltech.edu>
2017-02-14 00:00:26 -0800

    Added visualization

--- pandemaniac.py
+++ pandemaniac.py
@@ -1,6 +1,7 @@
 import json
 from pprint import pprint
 import networkx as nx
+import matplotlib.pyplot as plt
 
 def run(file_name):
     f = open(file_name, 'r')
@@ -10,16 +11,18 @@ def run(file_name):
     for k, v in data.iteritems():
         graph_data[int(k)] = [int(x) for x in v]
 
-    pprint(graph_data)
+    # pprint(graph_data)
 
-    # G = nx.Graph()
-    # for k, v in graph_data.iteritems():
-    #     for n in v:
-    #         G.add_edge(k, v)
+    G = nx.Graph()
+    for k, v in graph_data.iteritems():
+        for n in v:
+            G.add_edge(k, n)
 
 
     # pprint(graph_data)
-
+    # print G.nodes()
+    nx.draw(G)
+    plt.show()
 
 
 run('testgraph1.json')
