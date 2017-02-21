e7ef821763748db6c25bb8e16c5a54f360663cef
Ramsathwick Pathireddy <rppathir@caltech.edu>
2017-02-20 22:43:23 -0800

    Some testing

--- pandemaniac.py
+++ pandemaniac.py
@@ -6,6 +6,7 @@ import sim
 import numpy as np
 import collections
 import random
+import itertools
 
 def run(file_name):
     f = open(file_name, 'r')
@@ -29,8 +30,6 @@ def run(file_name):
 
 
 
-
-
     # pprint(graph_data)
     # print G.nodes()
     # nx.draw(G)
@@ -42,34 +41,47 @@ def run(file_name):
     # Strategy
     win = 0
     total = 0
-    for num_nodes in range(5, 30):
+    for num_nodes in range(10, 11):
         print 'Num nodes; ', num_nodes
         total += 1
-        # s1_nodes = get_highest_degree_nodes(num_nodes + num_nodes / 5, G)
+        s1_nodes = get_highest_degree_nodes(num_nodes, G)
+
 
         # s1_nodes = strategy_3(num_nodes, G)
         # s1_nodes = strategy_4(num_nodes, G)
         # s2_nodes = strategy_4(num_nodes, G)
-        s2_nodes = strategy_5(num_nodes, G) # Best strategy so far
+        # s2_nodes = strategy_5(num_nodes, G) # Best strategy so far
         # s2_nodes = strategy_6(num_nodes, G)
         # s2_nodes = strategy_7(num_nodes, G)
         # s2_nodes = strategy_8(num_nodes, G)
         # s2_nodes = strategy_2(num_nodes, G)
+        s2_nodes = strategy_9(num_nodes, G)
+
 
         # print 's1 nodes: ', s1_nodes
-        # print 's2 nodes: ', s2_nodes
-        assert len(s2_nodes) == num_nodes
-        assert not set(s2_nodes).issubset(s1_nodes)
+        print 's2 nodes: ', s2_nodes
+        # assert len(s2_nodes) == num_nodes
+        # assert not set(s2_nodes).issubset(s1_nodes)
+        # printIntersection(s1_nodes, s2_nodes)
+        # s1_nodes = [57, 54, 53, 18]
+        # s2_nodes = [60, 80]
+        # strategies = {'s1' : s1_nodes, 's2' : s2_nodes}
+        # output = sim.run(graph_data, strategies)
+        # print output
 
-        strategies = {'s1' : s1_nodes, 's2' : s2_nodes}
-        output = sim.run(graph_data, strategies)
-
-        if output['s2'] > output['s1']:
-            win += 1
-        print output
-        print 'Win: %d  Total: %d Perc: %f' % (win, total, float(win) / total)
+        # if output['s2'] > output['s1']:
+        #     win += 1
+        # print 'Win: %d  Total: %d Perc: %f' % (win, total, float(win) / total)
     # Make the degree histogram for G
     return G
+
+
+def printIntersection(s1_nodes, s2_nodes):
+    print "Getting intersection "
+    s2_nodes_diff = [val for val in s2_nodes if val not in s1_nodes]
+    s1_nodes_diff = [val for val in s1_nodes if val not in s2_nodes]
+    print "S1 Nodes: ", s1_nodes_diff
+    print "S2 Nodes: ", s2_nodes_diff
 
 
 def make_histogram(G):
@@ -109,6 +121,26 @@ def get_highest_degree_nodes(n, G):
     D = nx.degree(G)
     x = sorted(D, key = D.get, reverse = True)[:n]
     return x
+
+
+def brute_force(s1_nodes, other_nodes, graph_data):
+    invalid = [11, 108, 75, 2, 17, 111, 115, 113, 57, 54, 53, 18]
+    valid = [val for val in other_nodes if val not in invalid]
+    print "valid length: ", len(valid)
+
+    attempt = list(itertools.combinations(valid, 2))
+    print len(attempt)
+    s1_nodes = [75, 2, 17, 111, 115, 113, 57, 54, 53, 18]
+
+
+    for elem in attempt:
+        s2_nodes = elem
+        strategies = {'s1' : s1_nodes, 's2' : s2_nodes}
+        output = sim.run(graph_data, strategies)
+        print output, "\t", elem
+        if output['s2'] > output['s1']:
+            print "We did it"
+            break
 
 
 ''' Methods attempted: Degree Centrality, Closeness centrality,
@@ -159,7 +191,6 @@ def strategy_4(n, G):
 def strategy_5(n, G):
     data_info = nx.number_of_cliques(G)
     node_order = sorted(data_info, key=lambda x:data_info[x], reverse=True)
-
     return node_order[:n]
 
 
@@ -184,6 +215,43 @@ def strategy_8(n, G):
     x = sorted(D, key = D.get, reverse=True)[:n]
     return x
 
+
+def strategy_9(n, G):
+    # Get top 120% nodes
+    D = nx.degree(G)
+    x = sorted(D, key = D.get, reverse = True)
+    top_strat = x[:n + (n / 5)]
+    print top_strat
+
+
+    # Look at the top nodes neighbors and see the one list
+    # where there are not many of the top neighbors
+    # for elem in top_strat:
+    #     print "elem: ", elem, " and ", G.neighbors(elem)
+    #     [val for val in G.neighbors(elem) if val not in invalid]
+    
+
+    # Look at nodes whose neighbors are not in this list
+    # print nx.degree_histogram(G)
+    # # Start with betweenness
+    # betD = nx.betweenness_centrality(G)
+    # betX = sorted(D, key = D.get, reverse=True)
+    # for elem in betX:
+
+    data_info = nx.number_of_cliques(G)
+    node_order = sorted(data_info, key=lambda x:data_info[x], reverse=True)
+    new_list = [val for val in node_order if val not in top_strat]
+    return new_list[:n]
+
+
+    # intersect = G.neighbors(x[0])
+    # for i in range(1, 8):
+    #     neighbors2 = G.neighbors(x[i])
+    #     # print "neighbors1: ", intersect
+    #     print "neighbors2: ", neighbors2
+    #     intersect = [val for val in intersect if val in neighbors2 ]
+    #     # and val not in top_strat]
+    #     print "Intersect: ", intersect
 
 
 def output_file(n, G, file_name):
